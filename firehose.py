b442a02d2e51e1fd5bff2d664e31c4409cb5b82c
Sudarshan Patil <sudarshanpatil9594@gmail.com>
2026-09-10 19:18:11 +0530

    feat: initialize firehose function and order data

--- firehose.py
+++ firehose.py
@@ -5,3 +5,13 @@ import io
 
 ORDER_FORMAT = '<QQdIcc2x'
 ORDER_STRUCT = struct.Struct(ORDER_FORMAT)
+
+async def firehose(num_orders: int, buffer: io.BytesIO):
+    timestamp = int(time.time() * 1000)
+    order_id = 1000000
+    price = 45000.50
+    size = 100
+    side = b'B'
+    order_type = b'L'
+    
+    start_time = time.perf_counter()
